9054cac4dd269adf956b6b7b4a6b3f9ff71a8ebb
Adaspeaks <adaspeaks@gmail.com>
2021-08-05 00:01:26 -0700

    basic server setup

---
+++ server/seeders/seeds.js
@@ -0,0 +1,13 @@
+const db = require('../config/connection');
+
+// db.once('open', async () => {
+//     try {
+//                     ;
+  
+//       console.log('all done!');
+//       process.exit(0);
+//     } catch (err) {
+//       throw err;
+//     }
+//   });
+  
