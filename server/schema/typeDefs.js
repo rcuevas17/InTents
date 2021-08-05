93f15012196903010951d0e07f488483db066c3d
Adaspeaks <adaspeaks@gmail.com>
2021-08-05 00:00:15 -0700

    added in case we use apollo
    Co-authored-by: Zachary K

---
+++ server/schema/typeDefs.js
@@ -0,0 +1,47 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    bookCount: Int
+    savedBooks:[Book]
+  }
+  
+  type Book {
+    bookId: ID!
+    authors: [String]
+    title: String!
+    description: String!
+    image: String!
+    link: String!
+  }
+
+  type Auth {
+    token:ID!
+    user:User!
+  }
+
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(email: String!, username: String!, password:String!): Auth
+    saveBook(bookData:BookInput!): User
+    removeBook(bookId:ID!): User
+  }
+
+  input BookInput{
+    bookId: String
+    authors: [String]
+    title: String
+    description: String
+    image: String
+    link: String
+  }
+`;
+
+module.exports = typeDefs;
